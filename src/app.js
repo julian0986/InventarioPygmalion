0fa7486f8d47aa74509e4d6cc69eef0e6efc49f7
Julian Calle <julian0986@gmail.com>
2021-11-22 12:56:53 -0500

    Se crea la ruta principal.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const indexRoutes = require("./routes/index.route")
 
 const port = 3001;
 
+app.use(indexRoutes)
 app.listen(port, () =>{
     console.log(`Servidor escuchado en puerto ${port}`)
 })
+
+
 
