55f9a798da79d16b1b1f9280da1f02d1674e19b8
Julian Calle <julian0986@gmail.com>
2021-11-23 10:22:01 -0500

    Se realiza conexion a base de datos y se crean las variables de entorno

--- src/app.js
+++ src/app.js
@@ -4,6 +4,21 @@ const indexRoutes = require("./routes/index.route")
 
 const port = 3001;
 
+//conexion a db.
+
+const user = "facilitador6";
+const password = "PygmaFacili21Pyg";
+const dbname = "pygmalion";
+
+const uri =  `mongodb+srv://${user}:${password}@cluster0.jvjwk.mongodb.net/${dbname}?retryWrites=true&w=majority`
+
+const mongoose = require("mongoose");
+
+mongoose
+.connect(uri)
+.then(()=>{console.log("mongo conectado")})
+.catch((err)=>{console.log(err)});
+
 app.use(indexRoutes)
 app.listen(port, () =>{
     console.log(`Servidor escuchado en puerto ${port}`)
