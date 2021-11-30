764e617c5be9cba746161b4fd29d1d151b6ecceb
Jose Calderin <jcalderin@mobiletec.net>
2021-11-30 10:42:04 -0500

    se agregan acciones modificar y eliminar

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -4,8 +4,8 @@ const productos = require('../models/producto');
 exports.agregarProducto = async (req, res) => {
     const producto = new productos(req.body);
 
-    try{
-       await producto.save();
+    try {
+        await producto.save();
         res.status(200).send({
             message: 'Producto agregado correctamente'
         });
@@ -18,41 +18,41 @@ exports.agregarProducto = async (req, res) => {
 
 //mostrar producto
 exports.mostrarProducto = async (req, res, next) => {
-    try {
-        const producto = await productos.findById(req.params.idProducto);
-        res.render('mostrarProducto', {
+    try {       
+        const producto = await productos.findById(req.params.id);
+        res.render('crearproducto', {
             producto: producto
         });
-       
 
-    } catch (error) {   
+
+    } catch (error) {
         res.status(500).send(error);
         next();
     }
 }
 
 //mostrar productos
-exports.mostrarProductos = async (req, res,next) => {
-    
+exports.mostrarProductos = async (req, res, next) => {
+
     try {
-        const lsproducto = await productos.find();
-        console.log(lsproducto);
+        const lsproducto = await productos.find({});     
         res.render("productos", {
-            arrayproductos:lsproducto
+            arrayproductos: lsproducto
         })
 
-     //   res.json(lsproducto);
+        //   res.json(lsproducto);
     } catch (error) {
         console.log(error);
         next();
-     
+
     }
 }
 
 // modificar producto
 exports.modificarProducto = async (req, res) => {
     try {
-        const producto = await productos.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        const id = req.params.id;
+        const producto = await productos.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.status(200).send(producto);
     } catch (error) {
         res.status(400).send(error);
@@ -65,9 +65,7 @@ exports.modificarProducto = async (req, res) => {
 exports.eliminarProducto = async (req, res) => {
     try {
         await productos.findByIdAndDelete(req.params.id);
-        res.status(200).send({
-            message: 'Producto eliminado correctamente'
-        });
+        res.redirect("/productos")
     } catch (error) {
         res.status(400).send(error);
         next();
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,6 +2,12 @@ const express = require("express");
 const Producto = require("../models/producto");
 const productController = require("../controllers/productoController");
 const router = express.Router();
+const methodOverride = require("method-override");;
+
+router.use(methodOverride("_method",{
+    methods: ["POST","GET"]
+}));
+
 
 router.get("/", (req, res) => {
     /* res.send("Hola desde aquí") */
@@ -9,19 +15,31 @@ router.get("/", (req, res) => {
 })
 
 
-
-
 router.get("/productos", productController.mostrarProductos);
+
+router.get("/producto/:id", productController.mostrarProducto);
+
+router.delete("/producto/:id", productController.eliminarProducto);
+
 //llamar la ruta antesd del post
 router.get("/crear", (req, res) => {
     /* res.send("Hola desde aquí") */
-    res.render("crearproducto")
+    res.render("crearproducto",{
+        producto:null
+    })
 })
 //crear el post para enviar productos.
 router.post("/", async (req, res) =>{
     const body = req.body;
     try {
-        await Producto.create(body)
+        const id = body.id;
+        if(id){
+            const productoM = await Producto.findByIdAndUpdate(id, body, { new: true });
+        } else{
+
+            await Producto.create(body)
+
+        }
         res.redirect("/productos")
     } catch (error) {
         console.log(error)
