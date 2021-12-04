3a91d97673e04e5d0a41060f6abe28679d858696
Julian Calle <julian0986@gmail.com>
2021-12-04 14:50:06 -0500

    Productos queda listo

---
+++ src/public/js/buscador.js
@@ -0,0 +1,24 @@
+/* 
+
+const formulario = document.querySelector('#buscproducto');
+const boton = document.querySelector('#boton');
+const resultado = document.querySelector('#resultado');
+
+const filtrar = ()=>{
+    /* console.log(formulario.value) */
+/* 
+    resultado.innerHTML = '';
+    const texto = formulario.value.toLowerCase();
+    for(let producto of productos){
+        let nombre = producto.nombre.toLowerCase()
+        if(nombre.indexOf(texto) !== -1){
+            resultado.innerHTML +=`<li>${producto.nombre}</li>`
+
+        }
+    }
+    if(resultado.innerHTML ===''){
+        resultado.innerHTML += `<li>Producto no Encontrado</li>`
+        
+    }
+} */
+/* boton.addEventListener('click', filtrar) */
